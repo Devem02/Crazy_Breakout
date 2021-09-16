23d253627be390516501a8e0c944a3b69cc2012d
Devem02 <diegovm02@estudiantec.cr>
2021-09-15 21:44:03 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Crazy_Breakout.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Crazy_Breakout.dir/main.cpp.obj"
+  "Crazy_Breakout.exe"
+  "Crazy_Breakout.exe.manifest"
+  "Crazy_Breakout.lib"
+  "Crazy_Breakout.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Crazy_Breakout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
