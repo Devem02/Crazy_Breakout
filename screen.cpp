e13f21a952934d5561c920cf86e2b2a24f558eca
Diego Vega <vega.diego02@gmail.com>
2021-09-21 11:29:03 -0600

    Primera Ventana

---
+++ screen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Diego on 20/09/2021.
+//
+
+#include "screen.h"
---
+++ screen.h
@@ -0,0 +1,14 @@
+//
+// Created by Diego on 20/09/2021.
+//
+
+#ifndef CRAZY_BREAKOUT_SCREEN_H
+#define CRAZY_BREAKOUT_SCREEN_H
+
+
+class screen {
+
+};
+
+
+#endif //CRAZY_BREAKOUT_SCREEN_H
